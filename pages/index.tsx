4997d461b9aa9895c95a4c8610483c4a432e3aa9
Josh Melo <joshuamelo1126@gmail.com>
2021-03-11 22:27:34 -0500

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className="border shadow">
+      Welcome to the Commsquare Frontend Repo! This will be deleted soon.
+    </div>
+  )
+}
