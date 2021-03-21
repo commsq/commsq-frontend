7c9383f2cc33d1d9706bf89b05ab70687867feeb
Josh <joshuamelo1126@gmail.com>
2021-03-20 20:59:42 -0400

    fixing env file

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,16 @@ import React from 'react'
 import Header from '../components/Header'
 import BuildingSearch from '../components/BuildingSearch'
 import Head from 'next/head'
+const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY
 
 const Home: React.FC = () => {
   return (
     <div className="flex flex-col h-screen">
       <Head>
-        <script async src="https://maps.googleapis.com/maps/api/js?key=&libraries=places"></script>
+        <script
+          async
+          src={`https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=places`}
+        ></script>
         <link
           href="https://fonts.googleapis.com/css?family=Montserrat:100,200,300"
           rel="stylesheet"
