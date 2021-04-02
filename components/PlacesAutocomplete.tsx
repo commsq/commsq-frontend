85d2d5f88bf332f1c36a5a0092d3ca4d27a18827
Josh Melo <joshuamelo1126@gmail.com>
2021-04-02 00:06:05 -0400

    worked on making keypresses work

--- components/PlacesAutocomplete.tsx
+++ components/PlacesAutocomplete.tsx
@@ -1,11 +1,14 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import usePlacesAutocomplete from 'use-places-autocomplete'
 import { db } from '../services/firebase'
 import { useRouter } from 'next/router'
 
 const PlacesAutocomplete: React.FC = () => {
   const router = useRouter()
+  const [highLightedIndex, setHighLightedIndex] = useState(null)
   const ref = useRef()
+  const UP_ARROW_KEY = 'ArrowUp'
+  const DOWN_ARROW_KEY = 'ArrowDown'
 
   const {
     ready,
@@ -16,6 +19,26 @@ const PlacesAutocomplete: React.FC = () => {
   } = usePlacesAutocomplete({
     debounce: 300,
   })
+
+  function handleKeyDown(event) {
+    let listElements = document.getElementsByClassName('suggestions')
+    if (listElements.length === 0) {
+      return
+    }
+
+    listElements = Array.from(listElements)
+    if (event.key === DOWN_ARROW_KEY) {
+      if (highLightedIndex === null) {
+        setHighLightedIndex(0)
+      } else {
+        listElements[highLightedIndex].style.backgroundColor = ''
+        setHighLightedIndex(highLightedIndex + 1)
+      }
+    } else if (event.key === UP_ARROW_KEY) {
+      listElements[highLightedIndex].style.backgroundColor = ''
+      setHighLightedIndex(highLightedIndex - 1)
+    }
+  }
 
   function handleInput(e) {
     setValue(e.target.value)
@@ -50,12 +73,27 @@ const PlacesAutocomplete: React.FC = () => {
         structured_formatting: { main_text, secondary_text },
       } = suggestion
       return (
-        <li key={place_id} onClick={handleSelect(suggestion)}>
+        <li className="suggestions" key={place_id} onClick={handleSelect(suggestion)}>
           <strong>{main_text}</strong> <small>{secondary_text}</small>
         </li>
       )
     })
   }
+
+  useEffect(() => {
+    let listElements = document.getElementsByClassName('suggestions')
+    if (listElements.length === 0) {
+      return
+    }
+
+    if (listElements[highLightedIndex] === undefined) {
+      return
+    }
+
+    listElements = Array.from(listElements)
+
+    listElements[highLightedIndex].style.backgroundColor = 'gray'
+  }, [highLightedIndex])
 
   return (
     <div ref={ref} className="w-full">
@@ -66,6 +104,7 @@ const PlacesAutocomplete: React.FC = () => {
         onChange={handleInput}
         disabled={!ready}
         placeholder="Enter your building address"
+        onKeyDown={(event) => handleKeyDown(event)}
       />
       {status === 'OK' && <ul>{renderSuggestions()}</ul>}
     </div>
